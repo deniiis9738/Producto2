5663799cd4e387b14e7da4ba5a8b8eebd3cbaa8a
Denis Andres Ion Badea <aluion9738@ieselcaminas.org>
2023-12-19 18:27:24 +0100

    creación de las clases de las tablas

---
+++ src/main/java/org/example/producto2/models/Pedido.java
@@ -0,0 +1,44 @@
+package org.example.producto2.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "direccion", length = 255, nullable = false)
+    private String direccion;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id", nullable = false)
+    private Usuario usuario;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+}
---
+++ src/main/java/org/example/producto2/models/Tipo.java
@@ -0,0 +1,43 @@
+package org.example.producto2.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tipo")
+public class Tipo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nombre", length = 50)
+    private String nombre;
+
+    @Column(name = "descripcion", length = 255)
+    private String descripcion;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
